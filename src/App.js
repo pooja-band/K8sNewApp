8cbd49eb94e05deff721ab8ea2671811d68a3c15
pooja-band <poojaband123@gmail.com>
2025-01-20 09:51:10 +0000

    committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Likes from './Likes';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const message=process.env.MESSAGE;
+  console.log(`Message :${message}`)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Welcome all...</h1>
+      <h2 className="text-center mb-4">Please hit the button below if you like the image.</h2>
+      <h1>Message is: {message}</h1>
+      <Likes value={0}/>
     </div>
   );
 }
