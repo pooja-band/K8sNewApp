e44d080dfd3805380a159f431e6a5e6d8c6d7cc4
pooja-band <poojaband123@gmail.com>
2025-01-21 07:42:15 +0000

    committed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Likes from './Likes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const message=process.env.MESSAGE;
-  console.log(`Message :${message}`)
+  // const message=process.env.MESSAGE;
+  // console.log(`Message :${message}`)
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">Welcome all...</h1>
       <h2 className="text-center mb-4">Please hit the button below if you like the image.</h2>
-      <h1>Message is: {message}</h1>
+      {/* <h1>Message is: {message}</h1> */}
       <Likes value={0}/>
     </div>
   );
